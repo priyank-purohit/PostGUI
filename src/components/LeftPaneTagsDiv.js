c78fe12a66b5d208beda474f72f70f842c913ab5
Priyank Purohit <priyankpurohit31@gmail.com>
2017-04-20 18:18:32 -0400

    Added AXIOS package + trying to make a GET req to / PostgREST endpoint

--- src/components/LeftPaneTagsDiv.js
+++ src/components/LeftPaneTagsDiv.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 var data = require('../data/data.json');
 var lib = require('../utils/library.js');
 
 class LeftPaneTagsDiv extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { tables: [] };
+	}
+
 	handleClick(e) {
 		var buttonClicked = e.target.id;
 		this.props.changeTargetTag(buttonClicked);
@@ -12,16 +19,30 @@ class LeftPaneTagsDiv extends Component {
 	getKeysFromJSON(jsonDataStr) {
 		var keys = [];
 		for (var k in jsonDataStr) {
- 			keys.push(<button key={k} id={k} className="tagsButton" onClick={this.handleClick.bind(this)}>{k}</button>);
+			keys.push(<button key={k} id={k} className="tagsButton" onClick={this.handleClick.bind(this)}>{k}</button>);
 			keys.push(<br key={k+1}/>);
 		}
 		return keys;
 	}
 
+	fetchDbTables(url = 'http://localhost:3001/') {
+		axios.get(url, {
+				params: {
+					ID: 12345
+				}
+			})
+			.then(function(response) {
+				console.log("le resp = " + JSON.stringify(response.data));
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
+
 	render() {
 		return (
 			<div id="tagsDiv">
-				{this.getKeysFromJSON(data)}
+				{this.fetchDbTables()}
 			</div>
 		);
 	}
