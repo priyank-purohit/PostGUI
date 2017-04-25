c9130dce17b9fb9091d71184e75a64f578c3d4cc
Priyank Purohit <priyankpurohit31@gmail.com>
2017-04-25 16:40:58 -0400

    config file values retreival function

--- src/utils/library.js
+++ src/utils/library.js
@@ -1,5 +1,12 @@
 exports.rot13ED = function(str) {
 	// eslint-disable-next-line
-	return str;
-	//return str.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+	//return str;
+	return str.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+}
+
+// Retrieves value of key from the config file
+exports.getFromConfig = function(key = "title") {
+	let configFile = require('../data/config.json');
+	let config = JSON.parse(JSON.stringify(configFile));
+	return config[key]
 }
