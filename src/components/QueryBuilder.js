f565979769ef1dbf1ef8b975b6b3b5a05268fe15
Priyank Purohit <priyankpurohit31@gmail.com>
2017-05-09 15:56:57 -0400

    parsing rules + translating operator from QB to PostgREST format + making HTTP req and logging response

--- src/components/QueryBuilder.js
+++ src/components/QueryBuilder.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 let lib = require('../utils/library.js');
 
@@ -32,7 +33,7 @@ export default class QueryBuilderWrapper extends React.Component {
 
     // Called when new props are received by the QB component
     componentWillReceiveProps(newProps) {
-        this.setState({table: newProps.table, columns: newProps.columns});
+        this.setState({ table: newProps.table, columns: newProps.columns });
         if (newProps.table && newProps.columns) {
             const element = this.refs.queryBuilder;
             this.rebuildQueryBuilder(element, newProps.table, newProps.columns);
@@ -45,8 +46,98 @@ export default class QueryBuilderWrapper extends React.Component {
 
         const rules = newRules ? newRules : defaultRules;
         const filters = lib.getQBFilters(table, columns);
-        
+
         window.$(element).queryBuilder({ filters, rules });
+    }
+
+    isArray(what) {
+        return Object.prototype.toString.call(what) === '[object Array]';
+    }
+
+    // this whole method will be redone when OR and complex logic is possible...
+    extractRules(rules) {
+        let plainArray = [];
+        if (!this.isArray(rules)) {
+            for (let key in rules) {
+                if (rules.hasOwnProperty(key)) {
+                    let val = rules[key];
+                    if (key === "id") {
+                        plainArray.push([rules['id'], rules['operator'], rules['value']]);
+                    } else if (key === "rules") {
+                        plainArray.push(this.extractRules(rules[key]));
+                    }
+                }
+            }
+        } else {
+            for (let i = 0; i < rules.length; i++) {
+                plainArray.push(this.extractRules(rules[i]));
+            }
+        }
+        return plainArray;
+    }
+
+    flatten(array, mutable) {
+        var toString = Object.prototype.toString;
+        var arrayTypeStr = '[object Array]';
+
+        var result = [];
+        var nodes = (mutable && array) || array.slice();
+        var node;
+
+        if (!array.length) {
+            return result;
+        }
+
+        node = nodes.pop();
+
+        do {
+            if (toString.call(node) === arrayTypeStr) {
+                nodes.push.apply(nodes, node);
+            } else {
+                result.push(node);
+            }
+        } while (nodes.length && (node = nodes.pop()) !== undefined);
+
+        result.reverse(); // we reverse result to restore the original order
+        return result;
+    }
+
+    buildURL(rules) {
+        console.log(JSON.stringify(rules));
+        let url = lib.getFromConfig("baseUrl") + "/" + this.state.table + "?";
+        for (let i = 0; i < rules.length; i = i + 3) {
+            url += rules[i] + "=" + lib.translateOperatorToPostgrest(rules[i + 1]);
+            if (rules[i + 2] != null) {
+                url += "." + rules[i + 2];
+            }
+            if (i !== (rules.length - 3)) {
+                url += "&";
+            }
+        }
+        return url;
+    }
+
+    fetchOutput(url) {
+        console.log("Making HTTP request to get url = " + url);
+        axios.get(url, { params: {} })
+            .then((response) => {
+                console.log("RESPONSE = " + JSON.stringify(response.data));
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
+    processRules(rules) {
+        let extractedRules = this.extractRules(rules);
+        let flattenedRules = this.flatten(extractedRules);
+        let url = this.buildURL(flattenedRules);
+
+        if (url !== null) {
+            this.fetchOutput(url);
+        }
+
+        return url;
     }
 
     // get data from QB and pass it as react component state
@@ -65,8 +156,8 @@ export default class QueryBuilderWrapper extends React.Component {
     }
 
     handleSubmitClick() {
-        let rules = window.$(this.refs.queryBuilder).queryBuilder('getSQL');
-        window.alert(JSON.stringify(rules));
+        let rules = window.$(this.refs.queryBuilder).queryBuilder('getRules');
+        console.log(JSON.stringify(this.processRules(rules)));
     }
 
     render() {
@@ -84,7 +175,7 @@ export default class QueryBuilderWrapper extends React.Component {
                 </div>
                 <pre>
                     Component state:
-                    {JSON.stringify(this.state.rules, undefined, 2)}
+                    {JSON.stringify(this.state.rulesClean, undefined, 2)}
                 </pre>
             </div>
         );
--- src/utils/library.js
+++ src/utils/library.js
@@ -64,50 +64,65 @@ exports.getQBRules = function() {
 
 exports.getQBRulesDELETEME = function() {
 	return {
-	  "condition": "AND",
-	  "rules": [
-	    {
-	      "id": "feature_id",
-	      "field": "feature_id",
-	      "type": "string",
-	      "input": "text",
-	      "operator": "equal",
-	      "value": "1"
-	    },
-	    {
-	      "condition": "OR",
-	      "rules": [
-	        {
-	          "id": "feature_type",
-	          "field": "feature_type",
-	          "type": "string",
-	          "input": "text",
-	          "operator": "equal",
-	          "value": "1"
-	        },
-	        {
-	          "id": "feature_type",
-	          "field": "feature_type",
-	          "type": "string",
-	          "input": "text",
-	          "operator": "equal",
-	          "value": "2"
-	        }
-	      ]
-	    }
-	  ],
-	  "valid": true
+		"condition": "AND",
+		"rules": [{
+			"id": "feature_id",
+			"field": "feature_id",
+			"type": "string",
+			"input": "text",
+			"operator": "equal",
+			"value": "1"
+		}, {
+			"condition": "OR",
+			"rules": [{
+				"id": "feature_type",
+				"field": "feature_type",
+				"type": "string",
+				"input": "text",
+				"operator": "equal",
+				"value": "1"
+			}, {
+				"id": "feature_type",
+				"field": "feature_type",
+				"type": "string",
+				"input": "text",
+				"operator": "equal",
+				"value": "2"
+			}]
+		}],
+		"valid": true
 	};
 }
 
 exports.getQBFilters = function(table, columns) {
 	if (columns.length <= 0) {
-		return [{id: 'error', label: 'ERROR: select a view...', type: 'string'}];
+		return [{ id: 'error', label: 'ERROR: select a view...', type: 'string' }];
 	}
 
 	let plain_strings_query_builder = [];
 	for (let i = 0; i < columns.length; i++) {
-		plain_strings_query_builder.push({id: columns[i], label: columns[i], type: 'string'});
+		plain_strings_query_builder.push({ id: columns[i], label: columns[i], type: 'string', operators: ['equal', 'not_equal', 'greater', 'less', 'greater_or_equal', 'less_or_equal', 'is_not_null', 'in', 'is_null'] });
 	}
 	return plain_strings_query_builder;
+}
+
+exports.translateOperatorToPostgrest = function(operator) {
+	let dict = [
+		['equals', 'eq'],
+		['not_equal', 'neq'],
+		['greater', 'gt'],
+		['less', 'lt'],
+		['greater_or_equal', 'gte'],
+		['less_or_equal', 'lte'],
+		['is_not_null', 'not.is.null'],
+		['in', 'in'],
+		['is_null', 'is.null']
+	];
+
+	for (let i = 0; i < dict.length; i++) {
+		if (dict[i][0] === operator) {
+			return dict[i][1];
+		}
+	}
+	return "eq";
 }
