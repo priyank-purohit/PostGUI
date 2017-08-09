b71575d25e517365b282206042163e3ee6f93291
Priyank Purohit <priyankpurohit31@gmail.com>
2017-08-09 13:49:36 -0400

    Reformatted and cleaned up Appbar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,38 +6,39 @@ import Typography from 'material-ui/Typography';
 //import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import HomeIcon from 'material-ui-icons/Home';
 
 let lib = require('../utils/library.js');
 
 const styleSheet = createStyleSheet({
-    root: {
-        marginTop: 0,
-        width: '100%',
-    },
-    flex: {
-        flex: 1,
-    },
+	root: {
+		width: '100%',
+	},
+	flex: {
+		flex: 1,
+	},
 });
 
 class AppBarA extends Component {
-    render() {
-    	let classes = styleSheet;
-        return (
-            <div className={classes.root}>
-		      <AppBar position="static">
-		        <Toolbar>
-		          <IconButton color="contrast" aria-label="Menu">
-		            <MenuIcon />
-		          </IconButton>
-		          <Typography type="title" color="inherit" className={classes.flex}>
-		            {lib.getFromConfig("title")}
-		          </Typography>
-		          {/*<Button color="contrast">Login</Button>*/}
-		        </Toolbar>
-		      </AppBar>
-		    </div>
-        );
-    }
+	render() {
+		return (
+			<div className={styleSheet.root}>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton color="contrast" aria-label="Menu">
+							<MenuIcon />
+						</IconButton>
+						<Typography type="title" color="inherit" className={styleSheet.flex}>
+							{lib.getFromConfig("title")}
+						</Typography>
+						{/*<IconButton color="contrast" aria-label="Menu">
+							<HomeIcon />
+						</IconButton>*/}
+					</Toolbar>
+				</AppBar>
+			</div>
+		);
+	}
 }
 
 export default withStyles(styleSheet)(AppBarA);
