9b625c29373c64b8af4fd482c90e4d4de97c5a48
Priyank Purohit <priyankpurohit31@gmail.com>
2017-08-09 14:43:29 -0400

    MiddlePane reformatted

--- src/components/MiddlePane.js
+++ src/components/MiddlePane.js
@@ -7,49 +7,40 @@ import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-
 import Typography from 'material-ui/Typography';
 
 const styleSheet = createStyleSheet(theme => ({
-    root: theme.mixins.gutters({
-        paddingTop: 16,
-        paddingBottom: 16,
-    }),
-    middlePaperSection: {
-        width: '70%',
-        marginLeft: 350,
-        marginTop: -450
-    },
-    cardMarginLeft: { // For items within the same section
-      marginLeft: 16
-    },
-    cardMarginLeftTop: { // For a new section
-      marginLeft: 16,
-      marginTop: 16 // want a bit more space at top to clearly indicate new section...
-    },
+	root: theme.mixins.gutters({
+		paddingTop: 16,
+		paddingBottom: 16,
+	}),
+	middlePaperSection: {
+		width: '70%',
+		marginLeft: 350,
+		marginTop: -450
+	},
+	cardMarginLeft: { // For items within the same section
+		marginLeft: 16
+	},
+	cardMarginLeftTop: { // For a new section
+		marginLeft: 16,
+		marginTop: 16 // want a bit more space at top to clearly indicate new section...
+	},
 }));
 
 function PaperSheet(props) {
-    const classes = props.classes;
-    return (
-        <div className={classes.middlePaperSection}>
-        <Paper className={classes.root} elevation={4}>
-          <CardHeader
-                title="Table"
-                subheader="A somewhat short description for the table. Could include helpful info about the data, or how to query the table."
-              />
-          <Typography type="subheading" className={classes.cardMarginLeftTop}>
-            Query Builder
-          </Typography>
-          <Typography type="body1" className={classes.cardMarginLeftTop}>
-            Options
-          </Typography>
-          <Typography type="subheading" className={classes.cardMarginLeftTop}>
-            Query Response // Sample Data
-          </Typography>
-      </Paper>
-    </div>
-    );
+	const classes = props.classes;
+	return (
+		<div className={classes.middlePaperSection}>
+			<Paper className={classes.root} elevation={4}>
+				<CardHeader title="Table" subheader="A somewhat short description for the table. Could include helpful info about the data, or how to query the table." />
+				<Typography type="subheading" className={classes.cardMarginLeftTop}>Query Builder</Typography>
+				<Typography type="body1" className={classes.cardMarginLeftTop}>Options</Typography>
+				<Typography type="subheading" className={classes.cardMarginLeftTop}>Sample Data</Typography>
+			</Paper>
+		</div>
+	);
 }
 
 PaperSheet.propTypes = {
-    classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styleSheet)(PaperSheet);
