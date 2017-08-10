0d3474d0b958cea0a3e8561ebf689b9e56ffa042
unknown <priyankpurohit31@gmail.com>
2017-08-09 23:11:33 -0400

    Database picker changes based on cofnig.json

--- src/components/DbPicker.js
+++ src/components/DbPicker.js
@@ -4,23 +4,21 @@ import { withStyles, createStyleSheet } from 'material-ui/styles';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Menu, { MenuItem } from 'material-ui/Menu';
 
+let lib = require("../utils/library.js");
+
 const styleSheet = createStyleSheet(theme => ({
 	root: {
 		width: '99%'
 	},
 }));
 
-const databaseOptions = [
-	'P. syringae Database',
-	'Burkholderia Database',
-	'C. albicans Database',
-];
 
 class DbPicker extends Component {
 	state = {
 		anchorEl: undefined,
 		open: false,
 		selectedIndex: 0,
+		databases: []
 	};
 
 	button = undefined;
@@ -37,17 +35,26 @@ class DbPicker extends Component {
 		this.setState({ open: false });
 	};
 
+	// get a list of databases in the config.json
+	componentWillMount() {
+		let databasesMapped = [];
+		lib.getFromConfig("databases").map((obj, index) => databasesMapped[index] = obj.title);
+		this.setState({
+			databases: databasesMapped
+		});
+	}
+
 	render() {
 		const classes = this.props.classes;
 		return (
 			<div className={classes.root}>
 				<List>
 					<ListItem button aria-haspopup="true" aria-controls="lock-menu" aria-label="Database" onClick={this.handleClickListItem} >
-						<ListItemText primary="Database" secondary={databaseOptions[this.state.selectedIndex]} />
+						<ListItemText primary="Database" secondary={this.state.databases[this.state.selectedIndex]} />
 					</ListItem>
 				</List>
 				<Menu id="lock-menu" anchorEl={this.state.anchorEl} open={this.state.open} onRequestClose={this.handleRequestClose} >
-					{databaseOptions.map((option, index) =>
+					{this.state.databases.map((option, index) =>
 						<MenuItem key={option} selected={index === this.state.selectedIndex} onClick={event => this.handleMenuItemClick(event, index)} >
 							{option}
 						</MenuItem>
