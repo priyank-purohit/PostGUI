280803100423daf0e01676a0f5e87a26a9827367
unknown <priyankpurohit31@gmail.com>
2017-08-10 23:08:35 -0400

    RightPane only renders if a table is selected

--- src/components/RightPane.js
+++ src/components/RightPane.js
@@ -99,28 +99,39 @@ class RightPane extends Component {
         window.$(element).queryBuilder({ filters, rules });
     }
 
-	render() {
+    renderQueryBuilder() {
 		const classes = this.props.classes;
 		let tableRename = lib.getTableConfig(this.state.dbIndex, this.state.table, "rename");
 		let tableDisplayName = tableRename ? tableRename : this.state.table;
 
 		let tableDescription = lib.getTableConfig(this.props.dbIndex, this.props.table, "description") ? lib.getTableConfig(this.props.dbIndex, this.props.table, "description") : "";
+    	return (
+    		<Paper className={this.state.leftPaneVisibility ? classes.root : classes.rootInvisibleLeft} elevation={5}>
+				<CardHeader title={tableDisplayName} subheader={tableDescription} />
+
+				<Typography type="subheading" className={classes.cardMarginLeftTop}>Query Builder</Typography>
+				{/*<Paper className={classes.root} elevation={3}>*/}
+					<div id='query-builder' ref='queryBuilder'/>
+				{/*</Paper>*/}
+
+				<Typography type="body1" className={classes.cardMarginLeftTop}>Options</Typography>
+				<SubmitButton />
+				<TextField required id="rowLimit" label="Row-limit" defaultValue="10000" className={classes.textField && classes.cardMarginLeft} margin="normal" />
+
+				<Typography type="subheading" className={classes.cardMarginLeftTop}>Sample Data</Typography>
+			</Paper>
+		);
+    }
+
+    renderHelpfulMessage() {
+    	return null;
+    }
+
+	render() {
+		const classes = this.props.classes;
 		return (
 			<div className={classes.middlePaperSection}>
-				<Paper className={this.state.leftPaneVisibility ? classes.root : classes.rootInvisibleLeft} elevation={5}>
-					<CardHeader title={tableDisplayName} subheader={tableDescription} />
-
-					<Typography type="subheading" className={classes.cardMarginLeftTop}>Query Builder</Typography>
-					{/*<Paper className={classes.root} elevation={3}>*/}
-						<div id='query-builder' ref='queryBuilder'/>
-					{/*</Paper>*/}
-
-					<Typography type="body1" className={classes.cardMarginLeftTop}>Options</Typography>
-					<SubmitButton />
-					<TextField required id="rowLimit" label="Row-limit" defaultValue="10000" className={classes.textField && classes.cardMarginLeft} margin="normal" />
-
-					<Typography type="subheading" className={classes.cardMarginLeftTop}>Sample Data</Typography>
-				</Paper>
+				{this.state.table === "" ? this.renderHelpfulMessage() : this.renderQueryBuilder()}
 			</div>
 		);
 	}
