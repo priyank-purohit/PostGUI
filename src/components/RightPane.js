5abdc109a1d2b8eedf1d53cd6617e71f15a1bfbd
Priyank Purohit <priyankpurohit31@gmail.com>
2017-08-15 18:09:48 -0400

    Fetchoutput responds to state.table

--- src/components/RightPane.js
+++ src/components/RightPane.js
@@ -138,7 +138,7 @@ class RightPane extends Component {
 						submitSuccess: false 
 				}, () => {
 					// fetchout
-					this.fetchOutput("http://hopper.csb.utoronto.ca:3001/gene_feature");
+					this.fetchOutput("http://hopper.csb.utoronto.ca:3001/" + this.state.table);
 				});
 			});
 			return rules;
@@ -209,6 +209,7 @@ class RightPane extends Component {
 					<TextField disabled required id="rowLimit" label="Row-limit" defaultValue="10000" className={classes.textField && classes.cardMarginLeft} margin="normal" />
 
 					<Typography type="subheading" className={classes.cardMarginLeftTop}>Sample Data</Typography>
+					<Typography type="subheading" className={classes.cardMarginLeftTop}>Table = {this.state.table}</Typography>
 
 					<Typography type="subheading" className={classes.cardMarginLeftTop}>{JSON.stringify(this.state.rawData)}</Typography>
 				</Paper>
