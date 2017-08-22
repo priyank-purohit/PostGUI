21df9b6926e271f2be4b0efd0dfa0fc297fa0cfd
Priyank Purohit <priyankpurohit31@gmail.com>
2017-08-22 16:00:06 -0400

    LeftPane.js no longer stores anything in its state, it sends all info up to App.js

--- src/components/LeftPane.js
+++ src/components/LeftPane.js
@@ -9,58 +9,17 @@ import DbSchema from './DbSchema.js'
 class LeftPane extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			dbIndex: props.dbIndex,
-			table: props.table,
-			columns: props.columns,
-			leftPaneVisibility: props.leftPaneVisibility
-		};
 	}
 
-	componentWillReceiveProps(newProps) {
-		this.setState({
-			leftPaneVisibility: newProps.leftPaneVisibility
-		});
-	}
-
-	// Changes the index of DB in state + App.js state
-	changeDbIndex(newIndex) {
-		this.props.changeDbIndex(newIndex);
-		this.setState({
-			dbIndex: newIndex
-		});
-	}
-
-
-	// Changes the index of DB in state + App.js state
-	changeTable(newTable) {
-		this.props.changeTable(newTable);
-		this.setState({
-			table: newTable
-		});
-	}
-
-	// Changes the index of DB in state + App.js state
-	changeColumns(newColumns) {
-		this.props.changeColumns(newColumns);
-		this.setState({
-			columns: newColumns
-		});
-	}
-
-	// Changes the index of DB in state + App.js state
-	changeVisibleColumns(newVisibleColumns) {
-		this.props.changeVisibleColumns(newVisibleColumns);
-	}
 
 	render() {
 		const classes = this.props.classes;
-		let rootClasses = this.state.leftPaneVisibility === true ? classes.root : classes.rootHide;
+		let rootClasses = this.props.leftPaneVisibility === true ? classes.root : classes.rootHide;
 		return (
 			<div className={rootClasses}>
-				<DbPicker changeDbIndex={this.changeDbIndex.bind(this)} dbIndex={this.state.dbIndex} table={this.state.table} leftPaneVisibility={this.state.leftPaneVisibility} />
+				<DbPicker changeDbIndex={this.props.changeDbIndex} dbIndex={this.props.dbIndex} table={this.props.table} leftPaneVisibility={this.props.leftPaneVisibility} />
 				<Divider />
-				<DbSchema changeTable={this.changeTable.bind(this)} changeColumns={this.changeColumns.bind(this)} changeVisibleColumns={this.changeVisibleColumns.bind(this)} changeDbIndex={this.changeDbIndex.bind(this)} dbIndex={this.state.dbIndex} table={this.state.table} columns={this.state.columns} leftPaneVisibility={this.state.leftPaneVisibility} />
+				<DbSchema changeTable={this.props.changeTable} changeColumns={this.props.changeColumns} changeVisibleColumns={this.props.changeVisibleColumns} changeDbIndex={this.props.changeDbIndex} dbIndex={this.props.dbIndex} table={this.props.table} columns={this.props.columns} leftPaneVisibility={this.props.leftPaneVisibility} />
 			</div>
 		);
 	}
