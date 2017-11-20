f08343f6684c36bb49ba4d31762ffd4b4c667e63
Priyank Purohit <priyankpurohit31@gmail.com>
2017-11-20 17:16:56 -0500

    Created a search bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
@@ -10,7 +11,7 @@ import HistoryIcon from 'material-ui-icons/History';
 
 let lib = require('../utils/library.js');
 
-
+//join: predicted genes, protein seqs
 class Navigation extends Component {
 	render() {
 		const classes = this.props.classes;
@@ -26,11 +27,14 @@ class Navigation extends Component {
 						<IconButton color="contrast" aria-label="Menu" onClick={this.props.toggleLeftPane.bind(this)}>
 							<MenuIcon />
 						</IconButton>
-						<Typography type="title" color="inherit" className={classes.flex}>
+						<Typography type="title" color="inherit" className={classes.dbTitleFlex}>
 							{dbTitle}
 						</Typography>
-						<IconButton color="contrast" aria-label="Menu" onClick={this.props.toggleHistoryPane.bind(this)}>
-							<HistoryIcon />
+						<div className={classes.searchBarFlex}>
+							<TextField id="search" placeholder="Search tables and columns" type="search" className={classes.searchBar} margin="normal" />
+						</div>
+						<IconButton className={classes.rightIconsFlex} color="contrast" aria-label="Menu" onClick={this.props.toggleHistoryPane.bind(this)}>
+							<HistoryIcon className={classes.floatRight} />
 						</IconButton>
 					</Toolbar>
 				</AppBar>
@@ -47,8 +51,33 @@ const styleSheet = {
 	root: {
 		width: '100%'
 	},
-	flex: {
-		flex: 1
+	dbTitleFlex: {
+		flex: 0.3
+	},
+	searchBarFlex: {
+		flex: 0.6,
+		display: 'block',
+		marginLeft: 5,
+    	marginRight: 5,
+    	marginTop: -10,
+	},
+	searchBar: {
+		width: 300,
+		marginLeft: 5,
+    	marginRight: 5,
+    	background: 'white',
+    	padding: 10,
+    	paddingBottom: 5,
+    	border: '1px solid grey',
+    	borderRadius: 3,
+    	float: 'right'
+	},
+	rightIconsFlex: {
+		flex: 0.1,
+		display: 'block'
+	},
+	floatRight: {
+		float: 'right'
 	}
 };
 
