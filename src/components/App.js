18e525bf1d12b7718f827e262520dfff5fa402b1
Priyank Purohit <priyankpurohit31@gmail.com>
2018-02-22 16:38:25 -0500

    Clean up

--- src/components/App.js
+++ src/components/App.js
@@ -109,61 +109,6 @@ export default class Layout extends React.Component {
 		});
 	}
 
-	// Takes the query part of the URL used to make PostgREST API call and converts to an array object that can be traversed
-	parseURLRules(urlQuery) {
-		if (urlQuery === null) {
-			return null;
-		}
-
-		urlQuery = urlQuery.replace(/not.and=\(/g, "(not.and,").replace(/not.or=\(/g, "(not.or,");
-		urlQuery = urlQuery.replace(/not.and\(/g, "(not.and,").replace(/not.or\(/g, "(not.or,");
-		urlQuery = urlQuery.replace(/and=\(/g, "(and,").replace(/or=\(/g, "(or,");
-		urlQuery = urlQuery.replace(/and\(/g, "(and,").replace(/or\(/g, "(or,");
-		
-		urlQuery = urlQuery.replace(/\(/g, "[").replace(/\)\s/g, "], ");
-		urlQuery = urlQuery.replace(/\)/g, "]");
-		urlQuery = urlQuery.replace(/\s+/, ", ");
-		urlQuery = "[" + urlQuery + "]";
-		urlQuery = urlQuery.replace(/[^[\],\s]+/g, "\"$&\"");
-		urlQuery = urlQuery.replace(/" /g, "\", ");
-
-		urlQuery = JSON.parse(urlQuery);
-		if (urlQuery.length === 1 && urlQuery[0] instanceof Array) {
-			urlQuery = urlQuery[0];
-		}
-		
-		return this.recursiveRulesCreation(urlQuery);
-	}
-
-	// Takes a tranversable array object and converts to jQB compliant JSON object
-	recursiveRulesCreation(arrayObj) {
-		let rules = {};
-		let rulesElement = [];
-		for (let i = 0; i < arrayObj.length; i++) {
-			if (i === 0) {
-				// Condition + Not + Valid
-				rules.condition = arrayObj[0].replace("not.","").toUpperCase();
-				rules.not = arrayObj[0].replace(".and", "").replace(".or", "") === "not";
-				rules.valid = true;
-			} else {
-				// Rules
-				if (arrayObj[i] instanceof Array) {
-					rulesElement.push(this.recursiveRulesCreation(arrayObj[i]));
-				} else {
-					let rule = arrayObj[i].split(".");
-					rulesElement.push({
-						id: rule[0],
-						field: rule[0],
-						operator: lib.translateOperatorTojQB(rule[1]),
-						value: rule[2]
-					});
-				}
-				rules.rules = rulesElement;
-			}
-		}
-		return rules;
-	}
-
 	toggleLeftPane() {
 		if (this.state.leftPaneVisibility) {
 			this.setState({
@@ -298,3 +243,58 @@ export default class Layout extends React.Component {
 
 const app = document.getElementById('root');
 ReactDOM.render(<Layout/>, app);
+
+	// Takes the query part of the URL used to make PostgREST API call and converts to an array object that can be traversed
+	/*parseURLRules(urlQuery) {
+		if (urlQuery === null) {
+			return null;
+		}
+
+		urlQuery = urlQuery.replace(/not.and=\(/g, "(not.and,").replace(/not.or=\(/g, "(not.or,");
+		urlQuery = urlQuery.replace(/not.and\(/g, "(not.and,").replace(/not.or\(/g, "(not.or,");
+		urlQuery = urlQuery.replace(/and=\(/g, "(and,").replace(/or=\(/g, "(or,");
+		urlQuery = urlQuery.replace(/and\(/g, "(and,").replace(/or\(/g, "(or,");
+		
+		urlQuery = urlQuery.replace(/\(/g, "[").replace(/\)\s/g, "], ");
+		urlQuery = urlQuery.replace(/\)/g, "]");
+		urlQuery = urlQuery.replace(/\s+/, ", ");
+		urlQuery = "[" + urlQuery + "]";
+		urlQuery = urlQuery.replace(/[^[\],\s]+/g, "\"$&\"");
+		urlQuery = urlQuery.replace(/" /g, "\", ");
+
+		urlQuery = JSON.parse(urlQuery);
+		if (urlQuery.length === 1 && urlQuery[0] instanceof Array) {
+			urlQuery = urlQuery[0];
+		}
+		
+		return this.recursiveRulesCreation(urlQuery);
+	}*/
+
+	// Takes a tranversable array object and converts to jQB compliant JSON object
+	/*recursiveRulesCreation(arrayObj) {
+		let rules = {};
+		let rulesElement = [];
+		for (let i = 0; i < arrayObj.length; i++) {
+			if (i === 0) {
+				// Condition + Not + Valid
+				rules.condition = arrayObj[0].replace("not.","").toUpperCase();
+				rules.not = arrayObj[0].replace(".and", "").replace(".or", "") === "not";
+				rules.valid = true;
+			} else {
+				// Rules
+				if (arrayObj[i] instanceof Array) {
+					rulesElement.push(this.recursiveRulesCreation(arrayObj[i]));
+				} else {
+					let rule = arrayObj[i].split(".");
+					rulesElement.push({
+						id: rule[0],
+						field: rule[0],
+						operator: lib.translateOperatorTojQB(rule[1]),
+						value: rule[2]
+					});
+				}
+				rules.rules = rulesElement;
+			}
+		}
+		return rules;
+	}*/
