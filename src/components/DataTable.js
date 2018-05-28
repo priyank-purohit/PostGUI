821dd54c521f4edc750461509046d7b32b31a394
Priyank Purohit <priyankpurohit31@gmail.com>
2018-05-28 14:21:26 -0400

    Merged with delete row + brought it up-to-date for MaterialUI lib update

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -3,8 +3,8 @@ import ReactTable from 'react-table';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
-import Snackbar from 'material-ui/Snackbar';
-import IconButton from 'material-ui/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
 import Downloads from './Downloads.js';
