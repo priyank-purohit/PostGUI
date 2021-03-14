557509440aed3e2e2b3792e7e90a81f4de9ca470
Priyank Purohit <priyankpurohit31@gmail.com>
2021-03-13 23:00:01 -0500

    Fixed import issue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {App} from './components/app.tsx'
+import {PostGUI} from './components/app.tsx'
 
 const app = document.getElementById('root')
 
-ReactDOM.render(<App />, app)
+ReactDOM.render(<PostGUI />, app)
