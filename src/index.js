fd11e5ef3186c264c94ea903339ae3cb74ca2c64
Priyank Purohit <priyankpurohit31@gmail.com>
2021-06-14 09:30:31 -0400

    js -> tsx

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import {PostGUI} from './components/app.tsx'
-
-const app = document.getElementById('root')
-
-ReactDOM.render(<PostGUI />, app)
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { PostGUI } from './components/app';
+
+
+const app = document.getElementById('root')
+
+ReactDOM.render(<PostGUI />, app)
